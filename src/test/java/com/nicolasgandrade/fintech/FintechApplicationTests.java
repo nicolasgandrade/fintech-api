b72516897feeb8ad75f7a9069ee0305ddfea122c
nicolasgandrade <nicolasguerrero72@gmail.com>
2022-02-28 12:38:09 -0300

    inital commit

---
+++ src/test/java/com/nicolasgandrade/fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasgandrade.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
